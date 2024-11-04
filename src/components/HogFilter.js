4619d92cfdaa051c1a6c4e3b03db0a8ffc620307
augustinemeso <augustinemesobk@gmail.com>
2024-11-04 16:28:07 +0300

    add REACT-HOOKS-HOGWILD

---
+++ src/components/HogFilter.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function HogFilter({ onToggleGreased, showGreased }) {
+  return (
+    <button onClick={onToggleGreased}>
+      {showGreased ? "Show All Hogs" : "Show Greased Hogs Only"}
+    </button>
+  );
+}
+
+export default HogFilter;
